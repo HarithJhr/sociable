15eb67f96d8e0503f4a6fe3d4e0dda203e2043c9
Harith <harithjhr@gmail.com>
2025-05-17 18:01:57 +0800

    Initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,31 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={styles.title}>Test</Text>
+      <Link href="/profile">Go to Profile</Link>
+      <Link href="/notifications">Go to Notifications</Link>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: "red",
+    fontSize: 50,
+  },
+});
